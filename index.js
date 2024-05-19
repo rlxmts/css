1d8a9f3a9514697674e29b95376e6bec54b7c803
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-19 13:51:41 -0300

    :dizzy: Efeitos: Adicionando fade no slider e autoplay

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    //direction: 'vertical',
+    spaceBetween: 30,
+    effect: "fade",
     loop: true,
-  
-    // If we need pagination
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+
     pagination: {
       el: '.swiper-pagination',
     },
-  
-    // Navigation arrows
+
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
     },
