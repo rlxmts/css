059295993897c9a40fe62bdc2a6924f381ab8f3c
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-05-19 16:08:45 -0300

    :dizzy: animação: ampliando imagens auto

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const swiper = new Swiper('.swiper', {
     loop: true,
 
     autoplay: {
-        delay: 5000,
+        delay: 2000,
         disableOnInteraction: false,
       },
 
@@ -21,3 +21,5 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+  
